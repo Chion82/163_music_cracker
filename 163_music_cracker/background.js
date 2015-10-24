f9cb7dd026cd508cadf981d2957698496e7ba4ee
Chion Tang <sdspeed@sina.com>
2015-10-24 22:38:26 +0800

    Change js redir url

--- 163_music_cracker/background.js
+++ 163_music_cracker/background.js
@@ -10,7 +10,8 @@ function requestListener(details) {
    if (details.url.includes('pt_content_playlist.js')) {
 	  console.log('Redirecting request...');
 	  return {
-	      redirectUrl : 'http://localhost:3838/pt_content_playlist.js'
+	  		redirectUrl : 'https://cdn.rawgit.com/Chion82/163_music_cracker/d3696029a4b6677cb978d097a59b6e12b86489b3/pt_content_playlist.js'
+	      //redirectUrl : 'http://localhost:3838/pt_content_playlist.js'
 	  };
   }
 }
